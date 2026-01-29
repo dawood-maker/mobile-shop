0a5ca41f59ceeb2d3b5a34c423aef6d562325884
Your Name <youremail@example.com>
2026-01-29 14:43:08 +0500

    new coding html and MobileGrid

--- src/components/MobileGrid.jsx
+++ src/components/MobileGrid.jsx
@@ -6,7 +6,7 @@ function MobileGrid({ mobiles, addToCart }) {
       {/* Grid */}
       <div
         className="
-          grid gap-5
+          grid gap-10
           grid-cols-1
           sm:grid-cols-2
           md:grid-cols-3
